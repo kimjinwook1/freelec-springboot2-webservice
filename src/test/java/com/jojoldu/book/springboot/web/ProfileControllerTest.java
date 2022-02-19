aaef64184cc1bfb7a861c0a4d4da909b6c588b4a
kimjinwook1 <87925272+kimjinwook1@users.noreply.github.com>
2022-02-19 13:30:13 +0900

    profile 조회

---
+++ src/test/java/com/jojoldu/book/springboot/web/ProfileControllerTest.java
@@ -0,0 +1,37 @@
+package com.jojoldu.book.springboot.web;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ProfileControllerTest {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	void profile은_인증없이_호출된다() {
+		String expected = "default";
+
+		ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).isEqualTo(expected);
+
+
+	}
+
+}
